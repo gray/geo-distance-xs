fd4bb2e8c14171675383c9d4523d3f6707249223
gray <gray@cpan.org>
2009-04-07 04:28:33 -0700

    benchmark each of the forumla types

--- ex/benchmark.pl
+++ ex/benchmark.pl
@@ -3,31 +3,44 @@ use strict;
 use warnings;
 
 use Benchmark qw(cmpthese timethese);
-use Geo::Distance;
+use Geo::Distance::XS;
+
+# When benchmarking, need to have it call import/unimport before the
+# code is executed.
+my $orig_timethis_sub = \&Benchmark::timethis;
+{
+    no warnings 'redefine';
+    *Benchmark::timethis = sub {
+        my $sub = ('perl' eq $_[2] ? 'un' : '') . 'import';
+        Geo::Distance::XS->$sub();
+        $orig_timethis_sub->(@_);
+    };
+}
 
 my @coord = (-118.243103, 34.159545, -73.987427, 40.853293);
 
 my $geo = Geo::Distance->new;
-sub geo {
-    my $d = $geo->distance(mile => @coord)
-};
 
-# Need this mess so timethese can be used.
-{
-    my $orig_timethis_sub = \&Benchmark::timethis;
-    no warnings 'redefine';
-    *Benchmark::timethis = sub {
-        if ($_[2] eq 'xs') {
-            eval 'use Geo::Distance::XS';
-            *Benchmark::timethis = $orig_timethis_sub;
-        }
-        $orig_timethis_sub->(@_);            
-    };
+sub geo {
+    my $d = $geo->distance(mile => @coord);
 }
 
-my $benchmarks = timethese -1, {
-    perl => \&geo,
-    xs   => \&geo,
-};
+for my $formula (qw(hsin polar cos gcd mt tv)) {
+    print "---- [ Formula: $formula ] ------------------------------------\n";
 
-cmpthese $benchmarks;
+    $geo->formula($formula);
+
+    Geo::Distance::XS->unimport;
+    printf "perl - distance from LA to NY: %s miles\n", geo();
+    Geo::Distance::XS->import;
+    printf "xs   - distance from LA to NY: %s miles\n", geo();
+    print "\n";
+
+    my $benchmarks = timethese -1, {
+        perl => \&geo,
+        xs   => \&geo,
+    };
+
+    cmpthese $benchmarks;
+    print "\n";
+}
