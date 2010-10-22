1ff51151b8f6b9a15d5f62cd7686cbc19a2cc82e
gray <gray@cpan.org>
2010-10-21 20:11:51 -0700

    add GIS::Distance::Fast to the benchmark

--- lib/Geo/Distance/XS.pm
+++ lib/Geo/Distance/XS.pm
@@ -75,74 +75,92 @@ it is installed.
 =head1 PERFORMANCE
 
 This distribution contains a benchmarking script which compares
-C<Geo::Distance::XS> with C<Geo::Distance>.  These are the results on a
-MacBook 2GHz with Perl 5.8.9:
+C<Geo::Distance::XS> with C<Geo::Distance> and C<GIS::Distance::Fast>.
+These are the results on a MacBook 2GHz with Perl 5.12.2:
 
     ---- [ Formula: hsin ] ------------------------------------
-    perl - distance from LA to NY: 2443.08796228363 miles
-    xs   - distance from LA to NY: 2443.08796228363 miles
+    perl     - distance from LA to NY: 2443.08796228363 miles
+    xs       - distance from LA to NY: 2443.08796228363 miles
+    gis_fast - distance from LA to NY: 2443.08796228363 miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  1 wallclock secs ( 1.09 usr +  0.01 sys =  1.10 CPU) @ 65162.73/s (n=71679)
-            xs:  2 wallclock secs ( 1.11 usr +  0.02 sys =  1.13 CPU) @ 936865.49/s (n=1058658)
-            Rate  perl    xs
-    perl  65163/s    --  -93%
-    xs   936865/s 1338%    --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  1 wallclock secs ( 1.06 usr +  0.01 sys =  1.07 CPU) @ 23642.99/s (n=25298)
+        perl:  1 wallclock secs ( 1.11 usr +  0.00 sys =  1.11 CPU) @ 77490.99/s (n=86015)
+            xs:  1 wallclock secs ( 1.00 usr +  0.01 sys =  1.01 CPU) @ 1238753.47/s (n=1251141)
+                Rate gis_fast     perl       xs
+    gis_fast   23643/s       --     -69%     -98%
+    perl       77491/s     228%       --     -94%
+    xs       1238753/s    5139%    1499%       --
 
     ---- [ Formula: polar ] ------------------------------------
-    perl - distance from LA to NY: 2766.02509696782 miles
-    xs   - distance from LA to NY: 2766.02509696782 miles
+    perl     - distance from LA to NY: 2766.02509696782 miles
+    xs       - distance from LA to NY: 2766.02509696782 miles
+    gis_fast - distance from LA to NY: 2766.02509696782 miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  1 wallclock secs ( 1.05 usr +  0.01 sys =  1.06 CPU) @ 67621.70/s (n=71679)
-            xs:  0 wallclock secs ( 1.06 usr + -0.00 sys =  1.06 CPU) @ 1180321.70/s (n=1251141)
-            Rate  perl    xs
-    perl   67622/s    --  -94%
-    xs   1180322/s 1645%    --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  1 wallclock secs ( 1.14 usr + -0.01 sys =  1.13 CPU) @ 19029.20/s (n=21503)
+        perl:  1 wallclock secs ( 1.07 usr +  0.00 sys =  1.07 CPU) @ 80387.85/s (n=86015)
+            xs:  2 wallclock secs ( 1.12 usr + -0.01 sys =  1.11 CPU) @ 1458670.27/s (n=1619124)
+                Rate gis_fast     perl       xs
+    gis_fast   19029/s       --     -76%     -99%
+    perl       80388/s     322%       --     -94%
+    xs       1458670/s    7565%    1715%       --
 
     ---- [ Formula: cos ] ------------------------------------
-    perl - distance from LA to NY: 2443.08796228363 miles
-    xs   - distance from LA to NY: 2443.08796228363 miles
+    perl     - distance from LA to NY: 2443.08796228363 miles
+    xs       - distance from LA to NY: 2443.08796228363 miles
+    gis_fast - distance from LA to NY: 2443.08796228363 miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  1 wallclock secs ( 1.02 usr +  0.01 sys =  1.03 CPU) @ 64238.83/s (n=66166)
-            xs:  2 wallclock secs ( 1.05 usr +  0.01 sys =  1.06 CPU) @ 927395.28/s (n=983039)
-            Rate  perl    xs
-    perl  64239/s    --  -93%
-    xs   927395/s 1344%    --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  1 wallclock secs ( 1.06 usr +  0.00 sys =  1.06 CPU) @ 23866.04/s (n=25298)
+        perl:  1 wallclock secs ( 1.03 usr +  0.00 sys =  1.03 CPU) @ 75918.45/s (n=78196)
+            xs:  2 wallclock secs ( 1.07 usr +  0.01 sys =  1.08 CPU) @ 1279826.85/s (n=1382213)
+                Rate gis_fast     perl       xs
+    gis_fast   23866/s       --     -69%     -98%
+    perl       75918/s     218%       --     -94%
+    xs       1279827/s    5263%    1586%       --
 
     ---- [ Formula: gcd ] ------------------------------------
-    perl - distance from LA to NY: 12438.0476860875-9076.08896733252i miles
-    xs   - distance from LA to NY: 2443.08796228363 miles
+    perl     - distance from LA to NY: 12438.0476860875-9076.08896733252i miles
+    xs       - distance from LA to NY: 2443.08796228363 miles
+    gis_fast - distance from LA to NY: 12438.0476860875-9076.08896733252i miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  2 wallclock secs ( 1.06 usr +  0.01 sys =  1.07 CPU) @ 5910.28/s (n=6324)
-            xs:  0 wallclock secs ( 1.07 usr +  0.00 sys =  1.07 CPU) @ 918728.04/s (n=983039)
-            Rate   perl     xs
-    perl   5910/s     --   -99%
-    xs   918728/s 15445%     --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  2 wallclock secs ( 1.07 usr +  0.00 sys =  1.07 CPU) @ 4185.98/s (n=4479)
+        perl:  1 wallclock secs ( 1.06 usr +  0.00 sys =  1.06 CPU) @ 6339.62/s (n=6720)
+            xs:  2 wallclock secs ( 1.05 usr +  0.00 sys =  1.05 CPU) @ 1310719.05/s (n=1376255)
+                Rate gis_fast     perl       xs
+    gis_fast    4186/s       --     -34%    -100%
+    perl        6340/s      51%       --    -100%
+    xs       1310719/s   31212%   20575%       --
 
     ---- [ Formula: mt ] ------------------------------------
-    perl - distance from LA to NY: 2443.08796228363 miles
-    xs   - distance from LA to NY: 2443.08796228363 miles
+    perl     - distance from LA to NY: 2443.08796228363 miles
+    xs       - distance from LA to NY: 2443.08796228363 miles
+    gis_fast - distance from LA to NY: 2443.08796228363 miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  1 wallclock secs ( 1.07 usr +  0.01 sys =  1.08 CPU) @ 56887.96/s (n=61439)
-            xs:  2 wallclock secs ( 0.99 usr +  0.01 sys =  1.00 CPU) @ 917503.00/s (n=917503)
-            Rate  perl    xs
-    perl  56888/s    --  -94%
-    xs   917503/s 1513%    --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  1 wallclock secs ( 1.13 usr +  0.01 sys =  1.14 CPU) @ 17148.25/s (n=19549)
+        perl:  1 wallclock secs ( 1.07 usr +  0.01 sys =  1.08 CPU) @ 66370.37/s (n=71680)
+            xs:  0 wallclock secs ( 1.07 usr +  0.01 sys =  1.08 CPU) @ 1274310.19/s (n=1376255)
+                Rate gis_fast     perl       xs
+    gis_fast   17148/s       --     -74%     -99%
+    perl       66370/s     287%       --     -95%
+    xs       1274310/s    7331%    1820%       --
 
     ---- [ Formula: tv ] ------------------------------------
-    perl - distance from LA to NY: 2448.24135235512 miles
-    xs   - distance from LA to NY: 2443.80013146211 miles
+    perl     - distance from LA to NY: 2448.24135235512 miles
+    xs       - distance from LA to NY: 2443.80013146211 miles
+    gis_fast - distance from LA to NY: 2448.24135235512 miles
 
-    Benchmark: running perl, xs for at least 1 CPU seconds...
-        perl:  2 wallclock secs ( 1.05 usr +  0.01 sys =  1.06 CPU) @ 13523.58/s (n=14335)
-            xs:  0 wallclock secs ( 1.05 usr + -0.01 sys =  1.04 CPU) @ 601509.62/s (n=625570)
-            Rate  perl    xs
-    perl  13524/s    --  -98%
-    xs   601510/s 4348%    --
+    Benchmark: running gis_fast, perl, xs for at least 1 CPU seconds...
+    gis_fast:  1 wallclock secs ( 1.05 usr +  0.01 sys =  1.06 CPU) @ 21353.77/s (n=22635)
+        perl:  1 wallclock secs ( 1.13 usr +  0.01 sys =  1.14 CPU) @ 15719.30/s (n=17920)
+            xs:  1 wallclock secs ( 1.04 usr +  0.00 sys =  1.04 CPU) @ 778425.00/s (n=809562)
+                Rate     perl gis_fast       xs
+    perl      15719/s       --     -26%     -98%
+    gis_fast  21354/s      36%       --     -97%
+    xs       778425/s    4852%    3545%       --
 
 =head1 SEE ALSO
 
@@ -189,10 +207,10 @@ L<http://search.cpan.org/dist/Geo-Distance-XS/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2009-2010 gray <gray at cpan.org>, all rights reserved.
 
-This library is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself.
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 =head1 AUTHOR
 
