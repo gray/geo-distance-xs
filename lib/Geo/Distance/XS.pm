b7f19cc91fef3ff7325c8df51a49c36c3338e4dc
gray <gray@cpan.org>
2011-08-01 14:44:58 -0700

    force usage of XSLoader

--- lib/Geo/Distance/XS.pm
+++ lib/Geo/Distance/XS.pm
@@ -3,20 +3,14 @@ package Geo::Distance::XS;
 use strict;
 use warnings;
 
+use Geo::Distance;
+use XSLoader;
+
 our $VERSION    = '0.08';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
-eval {
-    require XSLoader;
-    XSLoader::load(__PACKAGE__, $XS_VERSION);
-    1;
-} or do {
-    require DynaLoader;
-    DynaLoader::bootstrap(__PACKAGE__, $XS_VERSION);
-};
-
-use Geo::Distance;
+XSLoader::load(__PACKAGE__, $XS_VERSION);
 
 my ($orig_distance_sub, $orig_formula_sub);
 BEGIN {
